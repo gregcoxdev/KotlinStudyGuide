4b3258bf7888e7a9f32e27296c579d6ea423df3b
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-05 01:25:36 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,6 @@
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.max
 import kotlin.time.measureTime
 
 //TIP <b>1.5 One Away:</b> There are three types of edits that can be performed on strings: Insert a character, remove a
@@ -14,16 +17,50 @@ import kotlin.time.measureTime
 //<br>Input: firstString=["pale"], secondString=["bake"]
 //<br>Output: boolean=[False]
 fun main() {
-    val testCases = listOf(Pair("pale", "ple"), Pair("pales", "pale"), Pair("pale", "bale"), Pair("pale", "bake"), Pair("pale", "pl"), Pair("a", ""), Pair("a  ", "a"))
+    val testCases = listOf(Pair("pale", "ple"), Pair("pales", "pale"), Pair("pale", "bale"), Pair("pale", "bake"), Pair("pale", "pl"), Pair("a", ""), Pair("a  ", "a"), Pair("ple", "pale"), Pair("pale", "pale"), Pair("peal", "pale"))
     testCases.forEach { test ->
         val result: Boolean
         val duration = measureTime {
-            result = isOneAway(test.first, test.second)
+            result = isOneAway(test.first, test.second, 1)
         }
-        println("Test [$test] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
+        println("---> Test [$test] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
     }
 }
 
-fun isOneAway(firstString: String, secondString: String): Boolean {
-    return false
+fun isOneAway(firstString: String, secondString: String, numberOfDifferencesMax: Int): Boolean {
+
+    if (firstString == secondString) return true
+    val numberOfActualDifferences = when {
+        firstString.length == secondString.length -> checkReplacements(firstString, secondString)
+        firstString.length > secondString.length -> checkDifferences(firstString, secondString)
+        else -> checkDifferences(secondString, firstString)
+    }
+    println("Testing [$firstString] and [$secondString] resulted in $numberOfActualDifferences differences.")
+    return numberOfActualDifferences <= numberOfDifferencesMax
+}
+
+private fun checkReplacements(first: String, second: String): Int {
+    var numberOfActualDifferences = 0
+    first.forEachIndexed { index, char ->
+        if (char != second[index]) {
+            numberOfActualDifferences += 1
+        } else {
+            // No difference, continue.
+        }
+    }
+    return numberOfActualDifferences
+}
+
+private fun checkDifferences(first: String, second: String): Int {
+    var numberOfActualDifferences = 0
+    val secondCharList = second.toMutableList()
+    first.forEachIndexed { index, char ->
+        if (index < secondCharList.size && secondCharList[index] == char) {
+            // No difference here.
+        } else {
+            secondCharList.add(index, char)
+            numberOfActualDifferences += 1
+        }
+    }
+    return numberOfActualDifferences
 }
