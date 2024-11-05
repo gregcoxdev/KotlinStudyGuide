2a4cd92bbfc1d22388d1973e3f272ab6864e2905
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-04 23:27:20 -0800

    Updated base case

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import kotlin.time.measureTime
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+//TIP <b>1.1 Is Unique:</b> Implement an algorithm to determine if a string has all unique characters. What if you cannot
+//use additional data structures?
+fun main() {
+    val testCases = listOf("value1", "value2", "value3")
+    testCases.forEach { test ->
+        val result: Boolean
+        val duration = measureTime {
+            result = performTest(test)
+        }
+        println("Test [$test] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
     }
+}
+
+fun performTest(test: String): Boolean {
+    return false
 }
