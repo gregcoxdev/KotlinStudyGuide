1781ea62dacb6a9bd17a809572d1e57e88d7fb93
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-05 01:35:16 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -3,64 +3,21 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.time.measureTime
 
-//TIP <b>1.5 One Away:</b> There are three types of edits that can be performed on strings: Insert a character, remove a
-//character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits)
-//away.
-//<br><br>
-//TIP <b>EXAMPLE</b>
-//<br>Input: firstString=["pale"], secondString=["ple"]
-//<br>Output: boolean=[True]
-//<br>Input: firstString=["pales"], secondString=["pale"]
-//<br>Output: boolean=[True]
-//<br>Input: firstString=["pale"], secondString=["bale"]
-//<br>Output: boolean=[True]
-//<br>Input: firstString=["pale"], secondString=["bake"]
-//<br>Output: boolean=[False]
+//TIP <b>1.6 String Compression:</b> Implement a method to perform basic string compression using the counts of repeated
+//characters. For example, the string ["aabcccccaaa"] would become ["a2b1c5a3"]. If the "compressed" string would not
+//become smaller than the original string, your method should return the original string. You can assume the string has
+//only uppercase and lowercase letters (a-z).
 fun main() {
-    val testCases = listOf(Pair("pale", "ple"), Pair("pales", "pale"), Pair("pale", "bale"), Pair("pale", "bake"), Pair("pale", "pl"), Pair("a", ""), Pair("a  ", "a"), Pair("ple", "pale"), Pair("pale", "pale"), Pair("peal", "pale"))
+    val testCases = listOf(Pair("aabcccccaaa", "a2b1c5a3"), Pair("abcdefg", "abcdefg"), Pair("aBbC", "aBbC"), Pair("aabcccccCcccaaa", "a2b1c5C1c3a3"))
     testCases.forEach { test ->
         val result: Boolean
         val duration = measureTime {
-            result = isOneAway(test.first, test.second, 1)
+            result = stringCompressed(test.first) == test.second
         }
         println("---> Test [$test] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
     }
 }
 
-fun isOneAway(firstString: String, secondString: String, numberOfDifferencesMax: Int): Boolean {
-
-    if (firstString == secondString) return true
-    val numberOfActualDifferences = when {
-        firstString.length == secondString.length -> checkReplacements(firstString, secondString)
-        firstString.length > secondString.length -> checkDifferences(firstString, secondString)
-        else -> checkDifferences(secondString, firstString)
-    }
-    println("Testing [$firstString] and [$secondString] resulted in $numberOfActualDifferences differences.")
-    return numberOfActualDifferences <= numberOfDifferencesMax
-}
-
-private fun checkReplacements(first: String, second: String): Int {
-    var numberOfActualDifferences = 0
-    first.forEachIndexed { index, char ->
-        if (char != second[index]) {
-            numberOfActualDifferences += 1
-        } else {
-            // No difference, continue.
-        }
-    }
-    return numberOfActualDifferences
-}
-
-private fun checkDifferences(first: String, second: String): Int {
-    var numberOfActualDifferences = 0
-    val secondCharList = second.toMutableList()
-    first.forEachIndexed { index, char ->
-        if (index < secondCharList.size && secondCharList[index] == char) {
-            // No difference here.
-        } else {
-            secondCharList.add(index, char)
-            numberOfActualDifferences += 1
-        }
-    }
-    return numberOfActualDifferences
+fun stringCompressed(uncompressedString: String): String {
+    return ""
 }
