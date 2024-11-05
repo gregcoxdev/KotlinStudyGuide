728ecea4ece559df8123b3b2dcef3565e5c7ead4
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-04 23:38:51 -0800

    Solved for part 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,22 @@
 import kotlin.time.measureTime
 
-//TIP <b>Section & Test:</b> Add details of what should be tested in this section here, along with any examples.
+//TIP <b>1.1 Is Unique:</b> Implement an algorithm to determine if a string has all unique characters. What if you cannot
+//use additional data structures?
 fun main() {
-    val testCases = listOf("value1", "value2", "value3")
+    val testCases = listOf("", "a", "ab", "abba", "abc", "a?2", "abcdefgha")
     testCases.forEach { test ->
         val result: Boolean
         val duration = measureTime {
-            result = performTest(test)
+            result = isUnique(test)
         }
         println("Test [$test] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
     }
 }
 
-fun performTest(test: String): Boolean {
-    return false
+fun isUnique(test: String): Boolean {
+    val charSet: MutableSet<Char> = mutableSetOf()
+    test.forEach { char ->
+        if (charSet.contains(char)) return false else charSet.add(char)
+    }
+    return true
 }
