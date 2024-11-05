62a39a0c98638aa4535d21eef973b715e6705fed
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-04 23:47:10 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,28 @@
 import kotlin.time.measureTime
 
-//TIP <b>1.1 Is Unique:</b> Implement an algorithm to determine if a string has all unique characters. What if you cannot
-//use additional data structures?
+//TIP <b>1.2 Check Permutation:</b> Given two strings, write a method to decide if one is a permutation of the other.
 fun main() {
-    val testCases = listOf("", "a", "ab", "abba", "abc", "a?2", "abcdefgha")
+    val testCases = listOf(Pair("a", ""), Pair("a", "b"), Pair("ab", "a"), Pair("abc", "cab"), Pair("ab c", "abc"))
     testCases.forEach { test ->
         val result: Boolean
         val duration = measureTime {
-            result = isUnique(test)
+            result = isPermutation(test.first, test.second)
         }
         println("Test [$test] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
     }
 }
 
-fun isUnique(test: String): Boolean {
-    val charSet: MutableSet<Char> = mutableSetOf()
-    test.forEach { char ->
-        if (charSet.contains(char)) return false else charSet.add(char)
+fun isPermutation(firstString: String, secondString: String): Boolean {
+    if (firstString.length != secondString.length) return false
+    val firstCharMap: MutableMap<Char, Int> = mutableMapOf()
+    val secondCharMap: MutableMap<Char, Int> = mutableMapOf()
+    firstString.forEach { char ->
+        val occurrences = firstCharMap[char] ?: 0
+        firstCharMap[char] = occurrences + 1
     }
-    return true
+    secondString.forEach { char ->
+        val occurrences = secondCharMap[char] ?: 0
+        secondCharMap[char] = occurrences + 1
+    }
+    return firstCharMap == secondCharMap
 }
