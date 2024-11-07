5ced835da2a99289dbf29c3af0c15e864d1b0307
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-07 01:54:08 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,11 @@
 import kotlin.time.measureTime
 
-//TIP <b>3.1 Three in One:</b> Implement (or describe) three stacks using one array.
+//TIP <b>3.2 Stack Min:</b> Describe or implement how you would design a stack which, in addition to push and pop, has a
+// function min which returns the minimum element? Push, pop, and min should all operate in O(1) time.
 fun main() {
     var result: IntArray
     val durationSize = measureTime {
-        result = makeTripleStackArray()
+        result = makeMinStack()
     }
     println("Test completed with the result [$result] in ${durationSize.inWholeMilliseconds} milliseconds.")
 }
@@ -12,6 +13,6 @@ fun main() {
 /**
  * Explain rationale here.
  */
-fun makeTripleStackArray(): IntArray {
+fun makeMinStack(): IntArray {
     return intArrayOf()
 }
