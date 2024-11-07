d27d4414543d9b9fa00e157f986c1ee8e5768d15
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-06 20:31:40 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,23 @@
+import java.util.*
 import kotlin.time.measureTime
 
-//TIP <b>1.9 String Rotation:</b> Assume you have a method isSubstring which checks if one word is a substring of another.
-//Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
-//(e.g., "waterbottle" is a rotation of "erbottlewat").
+//TIP <b>2.1 Remove Dups:</b> Write code to remove duplicates from an unsorted linked list. As a follow-up, how would
+//you solve this problem if a temporary buffer is not allowed?
 fun main() {
-    val tests = listOf(Pair("waterbottle", "erbottlewat"), Pair("aa", "aa"), Pair("", ""), Pair("shirt", "pants"))
+    val tests = listOf(
+        createSinglyLinkedList(10, 3),
+        createSinglyLinkedList(3, 3),
+        createSinglyLinkedList(1, 1)
+    )
     tests.forEach { test ->
-        var result: Boolean? = null
+        var result: Node?
         val duration = measureTime {
-            result = isStringRotation(test.first, test.second)
+            result = removeDups(test)
         }
         println("Test [${test}] completed with the result [$result] in ${duration.inWholeMilliseconds} milliseconds.")
     }
 }
 
-fun isStringRotation(s1: String, s2: String): Boolean {
-    val s3 = s1 + s1
-    return s3.contains(s2)
+fun removeDups(headNode: Node?): Node? {
+    return null
 }
---
+++ src/Node.kt
@@ -0,0 +1,24 @@
+import kotlin.random.Random
+
+data class Node(val value: Int, var next: Node? = null)
+
+/**
+ * Generate a linked list with random values.
+ * @param size The size of the linked list, i.e. the number of values to store in the linked list.
+ * @param upperBounds The maximum generated value (non-inclusive), you'll have more duplicates the lower the value.
+ */
+fun createSinglyLinkedList(size: Int, upperBounds: Int = 3): Node? {
+    var headNode: Node? = null
+    var currentNode: Node? = null
+    for (i in 0..size) {
+        val nextNode = Node(Random.nextInt(from = 0, until = upperBounds))
+        if (headNode == null) {
+            headNode = nextNode
+            currentNode = headNode
+        } else {
+            currentNode?.next = nextNode
+            currentNode = currentNode?.next
+        }
+    }
+    return headNode
+}
