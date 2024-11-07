7c5c657e25cc263377964cbe63382d8de5614b2e
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-06 23:13:19 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -1,67 +1,44 @@
 import kotlin.time.measureTime
 
-//TIP <b>2.4 Partition:</b> Write code to partition a linked list around a value x, such that all nodes less than X come
-//before all nodes greater than or equal to X. (IMPORTANT: The partition element X can appear anywhere in the "right
-//partition"; it does not need t appear between the left and right partitions. The additional spacing in the example
-//below indicates the partition. Yes, the output below is one of many valid outputs.
+//TIP <b>2.5 Sum Lists:</b> You have two numbers represented by a linked list, where each node contains a single digit.
+//The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds
+//the two numbers and returns the sum as a linked list. (You are not allowed to "Cheat" and just convert the linked list
+//to an integer.)
 //<br><br>
 //EXAMPLE
 //<br><br>
-//Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
-//Output: [LEFT] 3 -> 1 -> 2 -> [RIGHT] 10 -> 5 -> 5 -> 8
+//Input: (7 -> 1 -> 6) + (5 -> 9 -> 2). That is, 617 + 295.
+//Output: 2 -> 1 -> 9. That is, 912.
+//<br><br>
+//FOLLOW-UP
+//<br><br>
+//Suppose the digits are stored in forward order. Repeat the above problem.
+//<br><br>
+//EXAMPLE
+//<br><br>
+//Input: (6 -> 1 -> 7) + (2 -> 9 -> 5). That is, 617 + 295.
+//Output: 9 -> 1 -> 2. That is, 912.
 fun main() {
     val tests = listOf(
-        Pair(createSinglyLinkedList(10, 10), 5),
-        Pair(createSinglyLinkedList(10, 10), 4),
-        Pair(createSinglyLinkedList(2, 100), 50),
-        Pair(createSinglyLinkedList(1, 100), 25)
+        Pair(createSinglyLinkedList(3, 10), createSinglyLinkedList(3, 10)),
+        Pair(createSinglyLinkedList(3, 10), createSinglyLinkedList(3, 10)),
+        Pair(createSinglyLinkedList(2, 10), createSinglyLinkedList(3, 10)),
+        Pair(createSinglyLinkedList(1, 10), createSinglyLinkedList(3, 10))
     )
     tests.forEach { test ->
         var result: Node?
         val durationSize = measureTime {
-            result = partition(test.first, test.second)
+            result = sumLists(test.first, test.second)
         }
         println("Test completed with the result [$result] in ${durationSize.inWholeMilliseconds} milliseconds.")
     }
 }
 
 /**
- * The train of thought behind this solution is to iterate through and
+ * Explain your train of thought.
  */
-fun partition(headNode: Node?, partitionValue: Int): Node? {
-    println("Test started with head node: $headNode")
-    var currentNode = headNode
-    var rightPartitionNode: Node? = null
-    var leftPartitionNode: Node? = null
-    var currentRightPartitionNode = rightPartitionNode
-    var currentLeftPartitionNode = leftPartitionNode
-    while (currentNode != null) {
-        if (currentNode.value >= partitionValue) {
-            println("Found node above or equal to $partitionValue. It's ${currentNode.value}.")
-            if (rightPartitionNode == null) {
-                rightPartitionNode = currentNode
-            } else {
-                currentRightPartitionNode?.next = currentNode
-            }
-            currentRightPartitionNode = currentNode
-        } else {
-            println("Found node less than $partitionValue. It's ${currentNode.value}.")
-            if (leftPartitionNode == null) {
-                leftPartitionNode = currentNode
-            } else {
-                currentLeftPartitionNode?.next = currentNode
-            }
-            currentLeftPartitionNode = currentNode
-        }
-        currentNode = currentNode.next
-    }
-    return when {
-        currentLeftPartitionNode == null -> rightPartitionNode
-        currentRightPartitionNode == null -> leftPartitionNode
-        else -> {
-            currentRightPartitionNode.next = null // Remove the rest of the old linked nodes.
-            currentLeftPartitionNode.next = rightPartitionNode // Combine the left and the right nodes.
-            return leftPartitionNode
-        }
-    }
+fun sumLists(firstLinkedList: Node?, secondLinkedList: Node?): Node? {
+    println("Test started with first node: $firstLinkedList")
+    println("Then adding node: $secondLinkedList")
+    return firstLinkedList
 }
