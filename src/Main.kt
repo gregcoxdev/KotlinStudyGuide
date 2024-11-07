8f4a5d8fb7c6229bc2028122add108e916aab881
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-06 21:42:14 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -19,5 +19,28 @@ fun main() {
 }
 
 fun removeDups(headNode: Node?): Node? {
+    println("Starting test for $headNode.")
+    val dataSet = mutableSetOf<Int>()
+    var currentNode = headNode
+    var prevNode: Node? = null
+    if (headNode == null) return null
+    while (currentNode != null) {
+        if (dataSet.contains(currentNode.value)) {
+            println("Value [${currentNode.value}] was already in the data set, removing $currentNode.")
+            prevNode?.next = currentNode.next // Set the previous next reference to this node's next reference, deleting this nodes reference.
+            currentNode = currentNode.next // Set the current node to the next one and repeat the loop.
+        } else {
+            println("Added [${currentNode.value}] to the data set.")
+            dataSet.add(currentNode.value)
+            prevNode = currentNode // Update the previous node to this one.
+            currentNode = currentNode.next // Set the current node to the next one and repeat the loop.
+        }
+    }
+    return headNode
+}
+
+fun removeDupsNoBuffer(headNode: Node?): Node? {
+    // Perform O(n^2) iterating through all the nodes taking a look at the value, then go through all the nodes again,
+    // deleting the nodes that match that value.
     return null
 }
