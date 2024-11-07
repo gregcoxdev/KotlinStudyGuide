1bb1924214bc88b67b281f22ba29bad4df3bc8b3
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-07 02:16:29 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,39 @@
 import kotlin.time.measureTime
 
-//TIP <b>3.2 Stack Min:</b> Describe or implement how you would design a stack which, in addition to push and pop, has a
-// function min which returns the minimum element? Push, pop, and min should all operate in O(1) time.
+//TIP <b>3.3 Stack of Plates:</b> Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+// Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement
+// a data structure SetOfStacks that mimics this. SetOfStacks should be composed of several stacks and should create a
+// new stack once the previous one exceeds capacity. SetOfStacks.push() and SetOfStacks.pop() should behave identically
+// to a single stack (that is, pop() should return the same values as it would if there were just a single stack).
+//<br><br>
+// FOLLOW-UP<br>
+// Implement a function popAt(index: Int) which performs a pop operation on a specific sub-stack.
 fun main() {
-    var result: IntArray
-    val durationSize = measureTime {
-        result = makeMinStack()
+    val duration = measureTime {
+        val setOfStacks = makeSetOfStacks()
+        for (i in 0..100) {
+            setOfStacks.push(i)
+        }
+        val value = setOfStacks.pop()
     }
-    println("Test completed with the result [$result] in ${durationSize.inWholeMilliseconds} milliseconds.")
+    println("Test completed in ${duration.inWholeMilliseconds} milliseconds.")
 }
 
 /**
  * Explain rationale here.
  */
-fun makeMinStack(): IntArray {
-    return intArrayOf()
+fun makeSetOfStacks(): SetOfStacks {
+    return SetOfStacks()
+}
+
+class SetOfStacks {
+
+    fun push(value: Int): Unit {
+
+    }
+
+    fun pop(): Int {
+        return -1
+    }
+
 }
