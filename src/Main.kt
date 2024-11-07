d2db7a2ec3b07322e58f0f6b215614b07115f747
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-07 02:07:38 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -11,7 +11,9 @@ fun main() {
 }
 
 /**
- * Explain rationale here.
+ * You could use a separate stack to track minimum value as you go. When a value is pushed, if it's less than or equal
+ * to the previous min, then push it to the min stack. When it's popped, you can pop the original stack and check if
+ * it's equal to min, if it's equal to min, then pop the min stack too. This should always keep the lowest on top.
  */
 fun makeMinStack(): IntArray {
     return intArrayOf()
