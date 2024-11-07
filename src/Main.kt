6ef36299587ab6e2cc8d28bf5a965fbc4b3ee9f0
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-07 00:31:34 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -18,9 +18,29 @@ fun main() {
 }
 
 /**
- * Explain rationale here.
+ * We have to iterate through the entire thing in order to tell. Since we don't have the length in a Linked List, we
+ * don't know when we should realistically begin mirroring to determine it's a palindrome.
  */
 fun isPalindrome(headNode: Node?): Boolean {
     println("Test started with head node [$headNode].")
-    return false
+    var currentNode = headNode
+    var palindromeStringBuilder = StringBuilder()
+    var size = 0
+    while (currentNode != null) {
+        size += 1
+        palindromeStringBuilder.append(currentNode.value)
+        currentNode = currentNode.next
+    }
+    if (size == 0 || size == 1) return true
+    val hasActualMidpoint = size % 2 == 1
+    val actualMidpoint = size.div(2)
+    if (hasActualMidpoint) {
+        val firstHalf = palindromeStringBuilder.substring(0, actualMidpoint)
+        val secondHalf = palindromeStringBuilder.substring(actualMidpoint + 1, size).reversed()
+        return firstHalf == secondHalf
+    } else {
+        val firstHalf = palindromeStringBuilder.substring(0, actualMidpoint)
+        val secondHalf = palindromeStringBuilder.substring(actualMidpoint, size).reversed()
+        return firstHalf == secondHalf
+    }
 }
