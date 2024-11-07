6cbcf56ba57aa4d07d2f1544ac2485fc5cf008cc
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-06 22:21:24 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -1,47 +1,24 @@
-import java.util.*
 import kotlin.time.measureTime
 
-//TIP <b>2.2 Return Kth to Last:</b> Implement an algorithm to find the kth to last element of a singly linked list.
+//TIP <b>2.3 Delete Middle Node:</b> Implement an algorithm to delete a node in the middle (i.e., any node but the first
+//and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
 fun main() {
     val tests = listOf(
-        Triple(createSinglyLinkedList(10, 500), 10, 2),
-        Triple(createSinglyLinkedList(3, 500), 3, 0),
-        Triple(createSinglyLinkedList(1, 500), 1, 1)
+        createSinglyLinkedList(9, 500),
+        createSinglyLinkedList(4, 500),
+        createSinglyLinkedList(2, 500),
+        createSinglyLinkedList(1, 500)
     )
     tests.forEach { test ->
-        var resultSize: Node?
+        var result: Node?
         val durationSize = measureTime {
-            resultSize = findKthToLastGivenSize(test.first, test.second, test.third)
+            result = deleteAnyMiddleNode(test)
         }
-        println("Test [${test.first}] completed with the result [$resultSize] in ${durationSize.inWholeMilliseconds} milliseconds.")
-
-        var resultNoSize: Node?
-        val durationNoSize = measureTime {
-            resultNoSize = findKthToLastGivenNoSize(test.first, test.third)
-        }
-        println("Test [${test}] completed with the result [$resultNoSize] in ${durationNoSize.inWholeMilliseconds} milliseconds.")
+        println("Test completed with the result [$result] in ${durationSize.inWholeMilliseconds} milliseconds.")
     }
 }
 
-fun findKthToLastGivenSize(headNode: Node?, size: Int, k: Int): Node? {
-    println("Starting test for $headNode.")
-    var currentNode = headNode
-    var index = 0
-    val targetIndex = size - k
-    while (currentNode != null) {
-        if (index == targetIndex) {
-            println("We are at the target. index[$index], value=[${currentNode.value}]")
-            break
-        } else {
-            currentNode = currentNode.next
-            index += 1
-        }
-    }
-    return currentNode
-}
-
-fun findKthToLastGivenNoSize(headNode: Node?, k: Int): Node? {
-    println("Starting test for $headNode.")
-    // Iterate through the entire list to get the size, then do the same thing given the size, this becomes O(n^2).
+fun deleteAnyMiddleNode(headNode: Node?): Node? {
+    println("Test started with headNode: $headNode")
     return headNode
 }
