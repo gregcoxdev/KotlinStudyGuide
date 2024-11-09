48f8a50438aee3362655cf1cd7750d948ab4613c
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-09 02:44:54 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -18,8 +18,36 @@ fun main() {
 }
 
 /**
- * Explain rationale here.
+ * Since we need to verify the root node, and then it's children to tell if it's a correct BST, we can use pre-order
+ * traversal.
  */
 private fun isBinarySearchTree(node: TreeNode): Boolean {
-    return false
+    println("Checking if this tree is a binary search tree..\n$node")
+    var isFormattedCorrectly = true
+    isFormattedCorrectly = preOrderTraversal(node)
+    println("Checked node=[${node.value}]. isFormattedCorrectly=[$isFormattedCorrectly]")
+    if (isFormattedCorrectly.not()) return isFormattedCorrectly
+    node.left?.let { left ->
+        isFormattedCorrectly = isBinarySearchTree(left)
+        println("Checking left=[${left.value}]. isFormattedCorrectly=[$isFormattedCorrectly]")
+        if (isFormattedCorrectly.not()) return isFormattedCorrectly
+    }
+    node.right?.let { right ->
+        isFormattedCorrectly = isBinarySearchTree(right)
+        println("Checking right=[${right.value}]. isFormattedCorrectly=[$isFormattedCorrectly]")
+        if (isFormattedCorrectly.not()) return isFormattedCorrectly
+    }
+    return isFormattedCorrectly
+}
+
+private fun preOrderTraversal(node: TreeNode?): Boolean {
+    if (node == null) return true
+    val isLeftNodeLessThanParent = node.left?.value?.let {
+        it <= node.value
+    } ?: true
+    val isRightNodeLessThanParent = node.right?.value?.let {
+        node.value <= it
+    } ?: true
+    println("Node with value ${node.value}, left=[${node.left?.value}=$isLeftNodeLessThanParent], right=[${node.right?.value}=$isRightNodeLessThanParent]")
+    return isLeftNodeLessThanParent && isRightNodeLessThanParent
 }
