0ec5a5901514eac09ae8652f1cc143416004ebc4
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-10 00:17:46 -0800

    Answered

--- src/Main.kt
+++ src/Main.kt
@@ -33,5 +33,59 @@ fun main() {
  * Write rationale here.
  */
 private fun findBuildOrder(projects: List<String>, dependencies: List<Pair<String, String>>): List<String> {
-    return listOf()
+    val projectToNodeMap = mutableMapOf<String, Node>()
+    val dependencyMap = mutableMapOf<String, Int>()
+
+    // Create nodes for each project.
+    projects.forEach {
+        projectToNodeMap[it] = Node(it)
+        dependencyMap[it] = 0
+    }
+
+    // Set up dependencies, in terms of nodes.
+    dependencies.forEach {
+        val (projectDependency, project) = it
+        val numberOfDependencies = dependencyMap[project]
+        dependencyMap[project] = if (numberOfDependencies == null) 1 else numberOfDependencies + 1
+        projectToNodeMap[project]?.let { projectNonNull ->
+            projectToNodeMap[projectDependency]?.let { projectDependencyNonNull ->
+                println("Added ${projectDependencyNonNull.value} as dependency for ${projectNonNull.value}")
+                projectDependencyNonNull.addChild(projectNonNull)
+            }
+        }
+    }
+
+    // Add all projects with zero dependencies to the list.
+    val projectBuildQueue = LinkedList<Node>()
+    dependencyMap.forEach { (project, dependencies) ->
+        if (dependencies == 0) {
+            projectToNodeMap[project]?.let { projectBuildQueue.add(it) }
+        }
+    }
+
+    val buildOrder = mutableListOf<String>()
+    if (projectBuildQueue.isEmpty()) {
+        println("There was a loop in the dependencies, returning null.")
+    } else {
+        while (projectBuildQueue.isNotEmpty()) {
+            val popped = projectBuildQueue.pop()
+            popped.isVisited = true
+            println("Popped ${popped.value}, adding ${popped.children}")
+            buildOrder.add(popped.value)
+            popped.children.forEach {
+                if (it.isVisited) {
+                    println("Project ${it.value} was already compiled, skipping.")
+                } else {
+                    it.isVisited = true
+                    projectBuildQueue.add(it)
+                }
+            }
+        }
+    }
+
+    if (buildOrder.size != projects.size) {
+        throw Exception("Circular dependency while building. Could only build the following: $buildOrder.")
+    }
+
+    return buildOrder
 }
