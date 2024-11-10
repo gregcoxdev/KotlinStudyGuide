8125810ccaccc4b69d0e9abdea0c2c4a191922bb
gregcoxdev <gregmcoxdev@gmail.com>
2024-11-09 16:11:04 -0800

    Unanswered

--- src/Main.kt
+++ src/Main.kt
@@ -8,46 +8,19 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.time.measureTime
 
-//TIP <b>4.5 Validate BST:</b> Implement a function to check if a binary tree is a binary search tree.
+//TIP <b>4.6 Successor:</b> Write an algorithm to find the "next" node (i.e., in order successor) of a given node in a
+// binary search tree. You may assume that each node has a link to its parent.
 fun main() {
-    var result: Boolean?
+    var result: TreeNode?
     val duration = measureTime {
-        result = isBinarySearchTree(node = createBinaryTree())
+        result = getSuccessor(node = createBinaryTree())
     }
     println("Test completed with $result in ${duration.inWholeMilliseconds} milliseconds.")
 }
 
 /**
- * Since we need to verify the root node, and then it's children to tell if it's a correct BST, we can use pre-order
- * traversal.
+ * Write rationale here.
  */
-private fun isBinarySearchTree(node: TreeNode): Boolean {
-    println("Checking if this tree is a binary search tree..\n$node")
-    var isFormattedCorrectly = true
-    isFormattedCorrectly = preOrderTraversal(node)
-    println("Checked node=[${node.value}]. isFormattedCorrectly=[$isFormattedCorrectly]")
-    if (isFormattedCorrectly.not()) return isFormattedCorrectly
-    node.left?.let { left ->
-        isFormattedCorrectly = isBinarySearchTree(left)
-        println("Checking left=[${left.value}]. isFormattedCorrectly=[$isFormattedCorrectly]")
-        if (isFormattedCorrectly.not()) return isFormattedCorrectly
-    }
-    node.right?.let { right ->
-        isFormattedCorrectly = isBinarySearchTree(right)
-        println("Checking right=[${right.value}]. isFormattedCorrectly=[$isFormattedCorrectly]")
-        if (isFormattedCorrectly.not()) return isFormattedCorrectly
-    }
-    return isFormattedCorrectly
-}
-
-private fun preOrderTraversal(node: TreeNode?): Boolean {
-    if (node == null) return true
-    val isLeftNodeLessThanParent = node.left?.value?.let {
-        it <= node.value
-    } ?: true
-    val isRightNodeLessThanParent = node.right?.value?.let {
-        node.value <= it
-    } ?: true
-    println("Node with value ${node.value}, left=[${node.left?.value}=$isLeftNodeLessThanParent], right=[${node.right?.value}=$isRightNodeLessThanParent]")
-    return isLeftNodeLessThanParent && isRightNodeLessThanParent
+private fun getSuccessor(node: TreeNode): TreeNode {
+    return node
 }
